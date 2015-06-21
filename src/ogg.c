5f57a78539124709e4d28b3d8e0f180aa68999ab
Simon Zolin <stsaz@yandex.ru>
2015-06-21 09:12:45 +0300

    ogg
    + encoding
    * using new improved ffogg_decode()
    + input: pass all vorbis tags to the core
    + input: configuration options

--- src/ogg.c
+++ src/ogg.c
@@ -1,4 +1,4 @@
-/** OGG Vorbis input.
+/** OGG Vorbis input/output.
 Copyright (c) 2015 Simon Zolin */
 
 #include <fmedia.h>
@@ -7,6 +7,8 @@ Copyright (c) 2015 Simon Zolin */
 #include <FF/audio/pcm.h>
 #include <FF/array.h>
 #include <FFOS/error.h>
+#include <FFOS/random.h>
+#include <FFOS/timer.h>
 
 
 static const fmed_core *core;
@@ -14,19 +16,32 @@ static uint status;
 
 typedef struct fmed_ogg {
 	ffogg og;
-	ffarr ileav;
 	uint state;
-	uint byte_rate;
-	uint64 curpos;
-	uint64 seekpos;
-	uint64 data_off;
-	uint64 first_gpos;
-	unsigned seek :1;
 } fmed_ogg;
 
-enum {
-	conf_seek = 1
+typedef struct ogg_out {
+	ffogg_enc og;
+	uint state;
+} ogg_out;
+
+static struct ogg_in_conf_t {
+	byte seekable;
+} ogg_in_conf;
+
+static struct ogg_out_conf_t {
+	uint qual;
+} ogg_out_conf;
+
+static const char *const metanames[] = {
+	"meta_album"
+	, "meta_artist"
+	, "meta_comment"
+	, "meta_date"
+	, "meta_genre"
+	, "meta_title"
+	, "meta_tracknumber"
 };
+
 
 //FMEDIA MODULE
 static const fmed_filter* ogg_iface(const char *name);
@@ -40,17 +55,35 @@ static const fmed_mod fmed_ogg_mod = {
 static void* ogg_open(fmed_filt *d);
 static void ogg_close(void *ctx);
 static int ogg_decode(void *ctx, fmed_filt *d);
+static int ogg_conf(ffpars_ctx *ctx);
 static const fmed_filter fmed_ogg_input = {
-	&ogg_open, &ogg_decode, &ogg_close
+	&ogg_open, &ogg_decode, &ogg_close, &ogg_conf
 };
 
-static int ogg_parse(fmed_ogg *o, fmed_filt *d);
-static int ogg_duration(fmed_ogg *o, fmed_filt *d);
-static void ogg_interleave(char *dst, const char *src, uint channels, size_t samples);
+static const ffpars_arg ogg_in_conf_args[] = {
+	{ "seekable",  FFPARS_TBOOL | FFPARS_F8BIT,  FFPARS_DSTOFF(struct ogg_in_conf_t, seekable) }
+};
+
+//ENCODE
+static void* ogg_out_open(fmed_filt *d);
+static void ogg_out_close(void *ctx);
+static int ogg_out_encode(void *ctx, fmed_filt *d);
+static int ogg_out_config(ffpars_ctx *ctx);
+static const fmed_filter fmed_ogg_output = {
+	&ogg_out_open, &ogg_out_encode, &ogg_out_close, &ogg_out_config
+};
+
+static const ffpars_arg ogg_out_conf_args[] = {
+	{ "quality",  FFPARS_TINT,  FFPARS_DSTOFF(struct ogg_out_conf_t, qual) }
+};
 
 
 FF_EXP const fmed_mod* fmed_getmod(const fmed_core *_core)
 {
+	fftime t;
+	fftime_now(&t);
+	ffrnd_seed(t.s);
+
 	ffmem_init();
 	core = _core;
 	return &fmed_ogg_mod;
@@ -59,8 +92,14 @@ FF_EXP const fmed_mod* fmed_getmod(const fmed_core *_core)
 
 static const fmed_filter* ogg_iface(const char *name)
 {
-	if (!ffsz_cmp(name, "decode"))
+	if (!ffsz_cmp(name, "decode")) {
+		ogg_in_conf.seekable = 1;
 		return &fmed_ogg_input;
+
+	} else if (!ffsz_cmp(name, "encode")) {
+		ogg_out_conf.qual = 50;
+		return &fmed_ogg_output;
+	}
 	return NULL;
 }
 
@@ -79,13 +118,25 @@ static void ogg_destroy(void)
 }
 
 
+static int ogg_conf(ffpars_ctx *ctx)
+{
+	ffpars_setargs(ctx, &ogg_in_conf, ogg_in_conf_args, FFCNT(ogg_in_conf_args));
+	return 0;
+}
+
 static void* ogg_open(fmed_filt *d)
 {
+	int64 input_size;
 	fmed_ogg *o = ffmem_tcalloc1(fmed_ogg);
 	if (o == NULL)
 		return NULL;
 	ffogg_init(&o->og);
-	o->state = 3;
+
+	if (FMED_NULL != (input_size = fmed_getval("total_size")))
+		o->og.total_size = input_size;
+
+	if (ogg_in_conf.seekable)
+		o->og.seekable = 1;
 	return o;
 }
 
@@ -93,114 +144,15 @@ static void ogg_close(void *ctx)
 {
 	fmed_ogg *o = ctx;
 	ffogg_close(&o->og);
-	ffarr_free(&o->ileav);
 	ffmem_free(o);
-}
-
-static int ogg_parse(fmed_ogg *o, fmed_filt *d)
-{
-	int r;
-	const char *name;
-
-	o->og.data = d->data;
-	o->og.datalen = d->datalen;
-
-	for (;;) {
-		r = ffogg_open(&o->og);
-		if (r < 0) {
-			errlog(core, d->trk, "ogg", "ffogg_open()");
-			return FMED_RERR;
-		}
-
-		switch (r) {
-		case FFOGG_RDONE:
-			dbglog(core, d->trk, "ogg", "vendor: %s", o->og.vcmt.vendor);
-			goto done;
-
-		case FFOGG_RMORE:
-			return FMED_RMORE;
-
-		case FFOGG_RTAG:
-			dbglog(core, d->trk, "ogg", "%S: %S", &o->og.tagname, &o->og.tagval);
-
-			name = NULL;
-			switch (ffogg_tag(o->og.tagname.ptr, o->og.tagname.len)) {
-			case FFOGG_TITLE:
-				name = "meta_title";
-				break;
-
-			case FFOGG_ARTIST:
-				name = "meta_artist";
-				break;
-			}
-
-			if (name != NULL)
-				d->track->setvalstr(d->trk, name, o->og.tagval.ptr);
-			break;
-		}
-	}
-
-done:
-	dbglog(core, d->trk, "ogg", "granule pos: %U", ffogg_granulepos(&o->og));
-
-	o->first_gpos = ffogg_granulepos(&o->og);
-	d->track->setval(d->trk, "pcm_format", FFPCM_FLOAT);
-	d->track->setval(d->trk, "pcm_channels", ffogg_channels(&o->og));
-	d->track->setval(d->trk, "pcm_sample_rate", ffogg_rate(&o->og));
-
-	o->data_off += d->datalen - o->og.datalen;
-
-	if (conf_seek) {
-		uint64 input_size = d->track->getval(d->trk, "total_size");
-		if (input_size != FMED_NULL) {
-			//read the last 4k to determine total duration
-			d->track->setval(d->trk, "input_seek", input_size - 4096);
-			o->state = 2;
-			return FMED_RMORE;
-		}
-	}
-
-	d->data = o->og.data;
-	d->datalen = o->og.datalen;
-	o->state = 0;
-	return FMED_ROK;
-}
-
-static int ogg_duration(fmed_ogg *o, fmed_filt *d)
-{
-	int r;
-	ffogg og2;
-	uint64 dur, trk_size;
-	uint brate;
-
-	ffogg_init(&og2);
-	og2.data = d->data;
-	og2.datalen = d->datalen;
-	r = ffogg_pageseek(&og2);
-	if (r <= 0) {
-		errlog(core, d->trk, "ogg", "pageseek failed");
-		goto done;
-	}
-
-	dur = (ffogg_granulepos(&og2) - o->first_gpos);
-	d->track->setval(d->trk, "total_samples", dur);
-
-	trk_size = d->track->getval(d->trk, "total_size");
-	brate = ffogg_bitrate(&o->og, dur, (trk_size != FMED_NULL) ? trk_size : 0);
-	o->byte_rate = brate / 8;
-	d->track->setval(d->trk, "bitrate", brate);
-
-done:
-	ffogg_close(&og2);
-	d->datalen = 0;
-	o->state = 0;
-	return FMED_ROK;
 }
 
 static int ogg_decode(void *ctx, fmed_filt *d)
 {
+	enum { I_HDR, I_DATA };
 	fmed_ogg *o = ctx;
 	int r;
+	uint tag;
 	int64 seek_time, until_time;
 
 	if (status == 1) {
@@ -208,119 +160,239 @@ static int ogg_decode(void *ctx, fmed_filt *d)
 		return FMED_RLASTOUT;
 	}
 
+	o->og.data = d->data;
+	o->og.datalen = d->datalen;
+
+again:
 	switch (o->state) {
-	case 3:
-		r = ogg_parse(o, d);
-		if (r != FMED_ROK)
-			return r;
+	case I_HDR:
 		break;
 
-	case 2:
-		r = ogg_duration(o, d);
-		if (r != FMED_ROK)
-			return r;
-		if (FMED_NULL == d->track->getval(d->trk, "seek_time")) {
-			d->track->setval(d->trk, "input_seek", o->data_off);
+	case I_DATA:
+		if (FMED_NULL != (seek_time = fmed_popval("seek_time")))
+			ffogg_seek(&o->og, ffpcm_samples(seek_time, ffogg_rate(&o->og)));
+		break;
+	}
+
+	for (;;) {
+		r = ffogg_decode(&o->og);
+		switch (r) {
+		case FFOGG_RMORE:
+			if (d->flags & FMED_FLAST) {
+				dbglog(core, d->trk, "ogg", "no eos page");
+				d->outlen = 0;
+				return FMED_RDONE;
+			}
 			return FMED_RMORE;
-		}
-		break;
-	}
 
-	seek_time = d->track->popval(d->trk, "seek_time");
-	if (seek_time != FMED_NULL) {
-		uint64 seek_bytes = o->byte_rate * seek_time / 1000;
-		dbglog(core, d->trk, "ogg", "seeking to %U sec (byte %U)", seek_time / 1000, seek_bytes);
-		d->track->setval(d->trk, "input_seek", seek_bytes - 4096);
-		o->seek = 1;
-		o->seekpos = ffpcm_samples(seek_time, ffogg_rate(&o->og));
-		return FMED_RMORE;
-	}
+		case FFOGG_RDATA:
+			goto data;
 
-	if (o->seek) {
-		//take granule pos from the previous page and then shift to the next page that is our target
-		o->seek = 0;
-		o->og.data = d->data;
-		o->og.datalen = d->datalen;
-		r = ffogg_pageseek(&o->og);
-		//note: we assume that all pages are aligned to 4k and input block size is larger
-		if (r <= 0) {
-			errlog(core, d->trk, "ogg", "pageseek failed");
+		case FFOGG_RDONE:
+			d->outlen = 0;
+			return FMED_RDONE;
+
+		case FFOGG_RHDR:
+			fmed_setval("pcm_format", FFPCM_FLOAT);
+			fmed_setval("pcm_channels", ffogg_channels(&o->og));
+			fmed_setval("pcm_sample_rate", ffogg_rate(&o->og));
+			fmed_setval("pcm_ileaved", 0);
+			break;
+
+		case FFOGG_RTAG:
+			dbglog(core, d->trk, "ogg", "%S: %S", &o->og.tagname, &o->og.tagval);
+
+			tag = ffogg_tag(o->og.tagname.ptr, o->og.tagname.len);
+			if (tag < FFCNT(metanames) && o->og.tagval.len != 0)
+				d->track->setvalstr(d->trk, metanames[tag], o->og.tagval.ptr);
+			break;
+
+		case FFOGG_RHDRFIN:
+			dbglog(core, d->trk, "ogg", "vendor: %s", o->og.vcmt.vendor);
+			if (!ogg_in_conf.seekable)
+				o->state = I_DATA;
+			break;
+
+		case FFOGG_RINFO:
+			fmed_setval("total_samples", o->og.total_samples);
+			fmed_setval("bitrate", ffogg_bitrate(&o->og));
+			o->state = I_DATA;
+			goto again;
+
+		case FFOGG_RSEEK:
+			fmed_setval("input_seek", o->og.off);
+			return FMED_RMORE;
+
+		case FFOGG_RWARN:
+			errlog(core, d->trk, "ogg", "warning: near sample %u: ffogg_decode(): %s"
+				, ffogg_cursample(&o->og), ffogg_errstr(o->og.err));
+			break;
+
+		default:
+			errlog(core, d->trk, "ogg", "ffogg_decode(): %s", ffogg_errstr(o->og.err));
 			return FMED_RERR;
 		}
-		d->data = o->og.data;
-		d->datalen = o->og.datalen;
-
-		o->curpos = ffogg_granulepos(&o->og);
-		dbglog(core, d->trk, "ogg", "seek diff: %D (%D ms)"
-			, o->curpos - o->seekpos, ffpcm_time(o->curpos - o->seekpos, ffogg_rate(&o->og)));
-		o->seekpos = 0;
-
-		o->og.data = d->data;
-		o->og.datalen = d->datalen;
-		r = ffogg_pageseek(&o->og);
-		if (r <= 0) {
-			errlog(core, d->trk, "ogg", "pageseek failed");
-			return FMED_RERR;
-		}
 	}
 
-	until_time = d->track->getval(d->trk, "until_time");
-	if (until_time != FMED_NULL) {
-		uint64 until_samples = (until_time / 1000) * ffogg_rate(&o->og);
-		if (until_samples <= o->curpos) {
+data:
+	if (FMED_NULL != (until_time = d->track->getval(d->trk, "until_time"))) {
+		uint64 until_samples = until_time * ffogg_rate(&o->og) / 1000;
+		if (until_samples <= ffogg_cursample(&o->og)) {
 			dbglog(core, d->trk, "ogg", "until_time is reached");
 			d->outlen = 0;
 			return FMED_RLASTOUT;
 		}
 	}
 
-	o->og.data = d->data;
-	o->og.datalen = d->datalen;
-	r = ffogg_decode(&o->og);
-	if (r < 0) {
-		errlog(core, d->trk, "ogg", "ffogg_decode()");
-		return FMED_RERR;
-	}
-	if (r == FFOGG_RMORE)
-		return FMED_RMORE;
-
 	dbglog(core, d->trk, "ogg", "decoded %u PCM samples, page: %u, granule pos: %U"
-		, o->og.nsamples, ogg_page_pageno(&o->og.opg), ffogg_granulepos(&o->og));
-	o->curpos += o->og.nsamples;
-	d->track->setval(d->trk, "current_position", o->curpos);
+		, o->og.nsamples, ffogg_pageno(&o->og), ffogg_granulepos(&o->og));
+	d->track->setval(d->trk, "current_position", ffogg_cursample(&o->og));
 
 	d->data = o->og.data;
 	d->datalen = o->og.datalen;
-
-	if (NULL == _ffarr_realloc(&o->ileav, o->og.nsamples, ffpcm_size(FFPCM_FLOAT, ffogg_channels(&o->og)))) {
-		syserrlog(core, d->trk, "ogg", "%e", FFERR_BUFALOC);
-		return FMED_RERR;
-	}
-	ogg_interleave(o->ileav.ptr, o->og.pcm, ffogg_channels(&o->og), o->og.nsamples);
-
-	d->out = o->ileav.ptr;
+	d->outni = (void**)o->og.pcm;
 	d->outlen = o->og.pcmlen;
-
-	if ((d->flags & FMED_FLAST) && d->datalen == 0)
-		return FMED_RDONE;
 	return FMED_ROK;
 }
 
-/*
-pcm[0][..] - left,  pcm[1][..] - right
-interleave: pcm[0,2..] - left */
-static void ogg_interleave(char *dst, const char *src, uint channels, size_t samples)
+
+static int ogg_out_config(ffpars_ctx *ctx)
 {
-	uint *out = (void*)dst;
-	const uint **in = (void*)src;
-	int n, ich, iff;
+	ffpars_setargs(ctx, &ogg_out_conf, ogg_out_conf_args, FFCNT(ogg_out_conf_args));
+	return 0;
+}
 
-	for (ich = 0;  ich != channels;  ich++) {
-		iff = ich;
+static void* ogg_out_open(fmed_filt *d)
+{
+	ogg_out *o = ffmem_tcalloc1(ogg_out);
+	if (o == NULL)
+		return NULL;
 
-		for (n = 0;  n < samples;  n++) {
-			out[iff] = in[ich][n];
-			iff += channels;
+	ffogg_enc_init(&o->og);
+	return o;
+}
+
+static void ogg_out_close(void *ctx)
+{
+	ogg_out *o = ctx;
+	ffogg_enc_close(&o->og);
+	ffmem_free(o);
+}
+
+static int ogg_out_addmeta(ogg_out *o, fmed_filt *d)
+{
+	uint i;
+	const char *val;
+	for (i = 0;  i < FFCNT(metanames);  i++) {
+		val = d->track->getvalstr(d->trk, metanames[i]);
+		if (val != FMED_PNULL) {
+			ffogg_iaddtag(&o->og, i, val);
 		}
 	}
+	return 0;
+}
+
+static int ogg_out_encode(void *ctx, fmed_filt *d)
+{
+	enum { I_CONF, I_CREAT, I_INPUT, I_ENCODE, I_DATA };
+	ogg_out *o = ctx;
+	int r, qual, il;
+	ffpcm fmt;
+
+	if (status == 1) {
+		d->outlen = 0;
+		errlog(core, d->trk, "ogg", "interrupted");
+		return FMED_RERR;
+	}
+
+	switch (o->state) {
+	case I_CONF:
+		fmt.format = (int)fmed_getval("pcm_format");
+		if (fmt.format != FFPCM_FLOAT)
+			fmed_setval("conv_pcm_format", FFPCM_FLOAT);
+
+		il = (int)fmed_getval("pcm_ileaved");
+		if (il != 0)
+			fmed_setval("conv_pcm_ileaved", 0);
+
+		if (fmt.format != FFPCM_FLOAT || il != 0) {
+			o->state = I_CREAT;
+			return FMED_RMORE;
+		}
+		//break;
+
+	case I_CREAT:
+		fmt.format = (int)fmed_getval("pcm_format");
+		il = (int)fmed_getval("pcm_ileaved");
+		if (fmt.format != FFPCM_FLOAT || il != 0) {
+			errlog(core, d->trk, "ogg", "input format must be PCM-float non-interleaved");
+			return FMED_RERR;
+		}
+
+		fmt.sample_rate = (int)fmed_getval("pcm_sample_rate");
+		fmt.channels = (int)fmed_getval("pcm_channels");
+
+		ogg_out_addmeta(o, d);
+
+		qual = (int)fmed_getval("ogg-quality");
+		if (qual == FMED_NULL)
+			qual = ogg_out_conf.qual;
+
+		if (0 != (r = ffogg_create(&o->og, &fmt, qual))) {
+			errlog(core, d->trk, "ogg", "ffogg_create() failed: %s", ffogg_errstr(r));
+			return FMED_RERR;
+		}
+		//o->state = I_INPUT;
+		//break;
+
+	case I_INPUT:
+		o->og.pcm = (const float**)d->data;
+		o->og.pcmlen = d->datalen;
+		o->state = I_ENCODE;
+		//break;
+
+	case I_ENCODE:
+		break;
+
+	case I_DATA:
+		d->out = o->og.data;
+		d->outlen = o->og.datalen;
+		o->state = I_ENCODE;
+		return FMED_ROK;
+	}
+
+	for (;;) {
+		r = ffogg_encode(&o->og);
+		switch (r) {
+
+		case FFOGG_RDATA:
+			goto data;
+
+		case FFOGG_RDONE:
+			d->outlen = 0;
+			return FMED_RDONE;
+
+		case FFOGG_RMORE:
+			if (!(d->flags & FMED_FLAST)) {
+				o->state = I_INPUT;
+				return FMED_RMORE;
+			}
+			//o->og.pcmlen == 0
+			break;
+
+		default:
+			errlog(core, d->trk, "ogg", "ffogg_encode() failed: %s", ffogg_errstr(o->og.err));
+			return FMED_RERR;
+		}
+	}
+
+data:
+	d->out = ffogg_pagehdr(&o->og, &d->outlen);
+	o->state = I_DATA;
+
+	dbglog(core, d->trk, "ogg", "output: %L+%L bytes, page: %u, granule pos: %U"
+		, (size_t)d->outlen, o->og.datalen
+		, ffogg_pageno(&o->og), ffogg_granulepos(&o->og));
+
+	return FMED_ROK;
 }
