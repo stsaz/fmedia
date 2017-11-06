fe3e84bd208cc3ad4277fb65d3c6aa364e965cac
Simon Zolin <stsaz@yandex.ru>
2017-11-06 11:44:25 +0300

    * AAC decode: detect real audio format (support implicit SBR, SBR+PS)

--- src/acodec/aac.c
+++ src/acodec/aac.c
@@ -95,8 +95,14 @@ static void aac_destroy(void)
 
 typedef struct aac_in {
 	ffaac aac;
+	uint state;
+	ffarr cache;
 	uint sample_rate;
+	uint frnum;
+	uint br;
 } aac_in;
+
+enum { DETECT_FRAMES = 16, }; //# of frames to detect real audio format
 
 static void* aac_open(fmed_filt *d)
 {
@@ -107,6 +113,7 @@ static void* aac_open(fmed_filt *d)
 	a->aac.enc_delay = fmed_popval_def(d, "audio_enc_delay", 0);
 	a->aac.end_padding = fmed_popval_def(d, "audio_end_padding", 0);
 	a->aac.total_samples = d->audio.total;
+	a->aac.contr_samprate = d->audio.fmt.sample_rate;
 	if (0 != ffaac_open(&a->aac, d->audio.fmt.channels, d->data, d->datalen)) {
 		errlog(core, d->trk, NULL, "ffaac_open(): %s", ffaac_errstr(&a->aac));
 		ffmem_free(a);
@@ -116,7 +123,6 @@ static void* aac_open(fmed_filt *d)
 	d->audio.fmt.format = a->aac.fmt.format;
 	d->audio.fmt.ileaved = 1;
 	a->sample_rate = d->audio.fmt.sample_rate;
-	d->audio.decoder = "AAC";
 	d->datatype = "pcm";
 	return a;
 }
@@ -125,20 +131,31 @@ static void aac_close(void *ctx)
 {
 	aac_in *a = ctx;
 	ffaac_close(&a->aac);
+	ffarr_free(&a->cache);
 	ffmem_free(a);
 }
 
+/*
+AAC audio format detection:
+. gather decoded frames in cache
+. when decoder notifies about changed audio format:
+ . set new audio format
+ . clear cached data
+. when the needed number of frames has been processed, start returning data
+If format changes again in the future, the change won't be handled.
+*/
 static int aac_decode(void *ctx, fmed_filt *d)
 {
 	aac_in *a = ctx;
+	int r;
+	enum { R_CACHE, R_CACHE_DATA, R_CACHE_DONE, R_PASS };
 
 	if (d->input_info)
 		return FMED_RDONE;
 
 	if (d->flags & FMED_FFWD) {
-		a->aac.data = d->data,  a->aac.datalen = d->datalen;
+		ffaac_input(&a->aac, d->data, d->datalen, d->audio.pos);
 		d->datalen = 0;
-		a->aac.cursample = d->audio.pos;
 	}
 
 	if ((d->flags & FMED_FFWD) && (int64)d->audio.seek != FMED_NULL) {
@@ -147,24 +164,83 @@ static int aac_decode(void *ctx, fmed_filt *d)
 		d->audio.seek = FMED_NULL;
 	}
 
-	int r;
+	for (;;) {
+
 	r = ffaac_decode(&a->aac);
 	if (r == FFAAC_RERR) {
 		errlog(core, d->trk, NULL, "ffaac_decode(): %s", ffaac_errstr(&a->aac));
 		return FMED_RERR;
 
 	} else if (r == FFAAC_RMORE) {
-		if (d->flags & FMED_FLAST) {
+		if (!(d->flags & FMED_FLAST))
+			return FMED_RMORE;
+		else if (a->cache.len == 0) {
 			d->outlen = 0;
 			return FMED_RDONE;
 		}
-		return FMED_RMORE;
+		a->state = R_CACHE_DATA;
+
+	} else {
+		dbglog(core, d->trk, NULL, "decoded %u samples (%U)"
+			, a->aac.pcmlen / ffpcm_size1(&a->aac.fmt), ffaac_cursample(&a->aac));
 	}
 
-	dbglog(core, d->trk, NULL, "decoded %u samples (%U)"
-		, a->aac.pcmlen / ffpcm_size1(&a->aac.fmt), ffaac_cursample(&a->aac));
-	d->audio.pos = ffaac_cursample(&a->aac);
+	switch (a->state) {
+	case R_CACHE:
+		if (r == FFAAC_RDATA_NEWFMT) {
+			ffpcmex *fmt = &d->audio.fmt;
+			fdkaac_info *inf = &a->aac.info;
+			dbglog(core, d->trk, NULL, "overriding audio configuration: %u/%u -> %u/%u"
+				, fmt->sample_rate, fmt->channels
+				, inf->rate, inf->channels);
+			if (fmt->sample_rate != inf->rate) {
+				if (d->audio.total != 0) {
+					d->audio.total *= a->aac.rate_mul;
+					a->aac.total_samples = d->audio.total;
+				}
+			}
+			a->sample_rate = fmt->sample_rate = inf->rate;
+			fmt->channels = inf->channels;
+			a->cache.len = 0;
+		}
+		a->br = ffint_mean_dyn(a->br, a->frnum, a->aac.info.bitrate);
+		if (NULL == ffarr_append(&a->cache, a->aac.pcm, a->aac.pcmlen))
+			return FMED_RSYSERR;
+		if (++a->frnum != DETECT_FRAMES)
+			continue;
+		//fall through
 
+	case R_CACHE_DATA:
+		d->audio.bitrate = a->br;
+		if (d->audio.total == 0 && (int64)d->input.size != FMED_NULL)
+			d->audio.total = d->input.size * 8 * a->sample_rate / a->br;
+		switch (a->aac.info.aot) {
+		case AAC_LC:
+			d->audio.decoder = "AAC-LC"; break;
+		case AAC_HE:
+			d->audio.decoder = "HE-AAC"; break;
+		case AAC_HEV2:
+			d->audio.decoder = "HE-AACv2"; break;
+		}
+
+		d->audio.pos = ffaac_cursample(&a->aac);
+		d->out = (void*)a->cache.ptr,  d->outlen = a->cache.len;
+		a->cache.len = 0;
+		a->state = R_CACHE_DONE;
+		return FMED_RDATA;
+
+	case R_CACHE_DONE:
+		ffarr_free(&a->cache);
+		a->state = R_PASS;
+		break;
+	}
+
+	if (a->state == R_PASS)
+		break;
+
+	}
+
+	d->audio.pos = ffaac_cursample(&a->aac);
 	d->out = (void*)a->aac.pcm,  d->outlen = a->aac.pcmlen;
 	return FMED_RDATA;
 }
