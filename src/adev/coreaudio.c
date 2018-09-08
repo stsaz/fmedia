a467acc2cf8b2a81527d31a8941478e4a53d1c26
Simon Zolin <stsaz@yandex.ru>
2018-09-08 10:32:31 +0300

    + coreaudio capture

--- src/adev/coreaudio.c
+++ src/adev/coreaudio.c
@@ -49,6 +49,30 @@ static const ffpars_arg coraud_out_conf_args[] = {
 	{ "device_index",	FFPARS_TINT,  FFPARS_DSTOFF(struct coraud_out_conf_t, idev) },
 };
 
+
+//INPUT
+static void* coraud_in_open(fmed_filt *d);
+static int coraud_in_read(void *ctx, fmed_filt *d);
+static void coraud_in_close(void *ctx);
+static int coraud_in_config(ffpars_ctx *ctx);
+static const fmed_filter fmed_coraud_in = {
+	&coraud_in_open, &coraud_in_read, &coraud_in_close
+};
+
+struct coraud_in_conf_t {
+	uint idev;
+	uint buflen;
+};
+static struct coraud_in_conf_t coraud_in_conf;
+
+static const ffpars_arg coraud_in_conf_args[] = {
+	{ "device_index",	FFPARS_TINT,  FFPARS_DSTOFF(struct coraud_in_conf_t, idev) },
+	{ "buffer_length",	FFPARS_TINT | FFPARS_FNOTZERO,  FFPARS_DSTOFF(struct coraud_in_conf_t, buflen) },
+};
+
+static void coraud_in_ontmr(void *param);
+
+
 //ADEV
 static int coraud_adev_list(fmed_adev_ent **ents, uint flags);
 static void coraud_adev_listfree(fmed_adev_ent *ents);
@@ -69,6 +93,8 @@ static const void* coraud_iface(const char *name)
 {
 	if (!ffsz_cmp(name, "out")) {
 		return &fmed_coraud_out;
+	} else if (!ffsz_cmp(name, "in")) {
+		return &fmed_coraud_in;
 	} else if (!ffsz_cmp(name, "adev")) {
 		return &fmed_coraud_adev;
 	}
@@ -79,6 +105,8 @@ static int coraud_conf(const char *name, ffpars_ctx *ctx)
 {
 	if (!ffsz_cmp(name, "out"))
 		return coraud_out_config(ctx);
+	else if (!ffsz_cmp(name, "in"))
+		return coraud_in_config(ctx);
 	return -1;
 }
 
@@ -380,4 +408,150 @@ static int coraud_write(void *ctx, fmed_filt *d)
 	}
 
 	return 0;
+}
+
+
+static int coraud_in_config(ffpars_ctx *ctx)
+{
+	coraud_in_conf.idev = 0;
+	coraud_in_conf.buflen = 1000;
+	ffpars_setargs(ctx, &coraud_in_conf, coraud_in_conf_args, FFCNT(coraud_in_conf_args));
+	return 0;
+}
+
+struct coraud_in {
+	void *trk;
+	ffcoraud_buf snd;
+	fftmrq_entry tmr;
+	uint64 total_samps;
+	uint frsize;
+	uint async :1;
+};
+
+static void* coraud_in_open(fmed_filt *d)
+{
+	struct coraud_in *a;
+	ffpcm fmt, in_fmt;
+	int r;
+	int dev_idx, dev_id = -1;
+
+	if (NULL == (a = ffmem_new(struct coraud_in)))
+		return NULL;
+
+	if (FMED_NULL != (int)(dev_idx = (int)d->track->getval(d->trk, "capture_device"))) {
+		dev_id = coraud_finddev(dev_idx, FFCORAUD_DEV_CAPTURE);
+		if (dev_id == -1) {
+			errlog("no audio device for capture by index #%u", dev_idx);
+			goto fail;
+		}
+	}
+
+	ffpcm_fmtcopy(&fmt, &d->audio.fmt);
+	in_fmt = fmt;
+
+	for (uint i = 0; ; i++) {
+
+		dbglog("opening device \"%d\", %s/%u/%u"
+			, dev_id, ffpcm_fmtstr(fmt.format), fmt.sample_rate, fmt.channels);
+		r = ffcoraud_open(&a->snd, dev_id, &fmt, coraud_in_conf.buflen, FFCORAUD_DEV_CAPTURE);
+
+		if (r == FFCORAUD_EFMT && i == 0
+			&& !!ffmemcmp(&fmt, &in_fmt, sizeof(fmt))) {
+
+			if (fmt.format != in_fmt.format) {
+				if (d->audio.convfmt.format == 0)
+					d->audio.convfmt.format = in_fmt.format;
+				d->audio.fmt.format = fmt.format;
+			}
+
+			if (fmt.sample_rate != in_fmt.sample_rate) {
+				if (d->audio.convfmt.sample_rate == 0)
+					d->audio.convfmt.sample_rate = in_fmt.sample_rate;
+				d->audio.fmt.sample_rate = fmt.sample_rate;
+			}
+
+			if (fmt.channels != in_fmt.channels) {
+				if (d->audio.convfmt.channels == 0)
+					d->audio.convfmt.channels = in_fmt.channels;
+				d->audio.fmt.channels = fmt.channels;
+			}
+
+			continue;
+		}
+		break;
+	}
+
+	if (r != 0) {
+		errlog("ffcoraud_open(): \"%d\": (%d) %s"
+			, dev_id, r, ffcoraud_errstr(r));
+		goto fail;
+	}
+
+	if (0 != (r = ffcoraud_start(&a->snd))) {
+		errlog("ffcoraud_start(): \"%d\": (%d) %s"
+			, dev_id, r, ffcoraud_errstr(r));
+		goto fail;
+	}
+
+	uint nfy_int_ms = ffpcm_bytes2time(&fmt, ffcoraud_bufsize(&a->snd) / 4);
+	a->tmr.handler = &coraud_in_ontmr;
+	a->tmr.param = a;
+	if (0 != core->timer(&a->tmr, nfy_int_ms, 0))
+		goto fail;
+
+	dbglog("opened capture buffer %ums"
+		, ffpcm_bytes2time(&fmt, ffcoraud_bufsize(&a->snd)));
+	a->frsize = ffpcm_size1(&fmt);
+	d->audio.fmt.ileaved = 1;
+	d->datatype = "pcm";
+	a->trk = d->trk;
+	return a;
+
+fail:
+	coraud_in_close(a);
+	return NULL;
+}
+
+static void coraud_in_close(void *ctx)
+{
+	struct coraud_in *a = ctx;
+	ffcoraud_close(&a->snd);
+	ffmem_free(a);
+}
+
+static void coraud_in_ontmr(void *param)
+{
+	struct coraud_in *a = param;
+	if (!a->async)
+		return;
+	a->async = 0;
+	track->cmd(a->trk, FMED_TRACK_WAKE);
+}
+
+static int coraud_in_read(void *ctx, fmed_filt *d)
+{
+	struct coraud_in *a = ctx;
+	int r;
+	ffstr data;
+
+	if (d->flags & FMED_FSTOP) {
+		ffcoraud_stop(&a->snd);
+		d->outlen = 0;
+		return FMED_RDONE;
+	}
+
+	r = ffcoraud_read(&a->snd, &data);
+	if (r < 0) {
+		errlog("ffcoraud_read(): (%d) %s", r, ffcoraud_errstr(r));
+		return FMED_RERR;
+	} else if (r == 0) {
+		a->async = 1;
+		return FMED_RASYNC;
+	}
+
+	d->out = data.ptr,  d->outlen = data.len;
+	dbglog("read %L bytes", d->outlen);
+	a->total_samps += d->outlen / a->frsize;
+	d->audio.pos = a->total_samps;
+	return FMED_RDATA;
 }
